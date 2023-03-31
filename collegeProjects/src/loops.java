16e335317d75715fe5526e23ab528c0d7c84e534
MacBook <macbook@Bibek-M1.local>
2023-03-31 08:23:56 +0545

    loops

---
+++ collegeProjects/src/loops.java
@@ -0,0 +1,19 @@
+// contains loops of java
+/* 
+ * 
+ */
+
+public class loops {
+    public static void main(String[] args){
+        for (int index= 0; index <= 50; index++){
+            // if(index == 10){
+            //     continue;
+            // }
+            // print even number 
+            if(index%2 == 0){
+                System.out.println(index);
+            }
+            
+        }
+    }
+}
