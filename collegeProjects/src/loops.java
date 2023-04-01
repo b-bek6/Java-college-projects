db36b828988cd6c1138df0cc61afc4da72d57168
MacBook <macbook@Bibek-M1.local>
2023-04-01 11:53:07 +0545

    java files

--- collegeProjects/src/loops.java
+++ collegeProjects/src/loops.java
@@ -5,15 +5,28 @@
 
 public class loops {
     public static void main(String[] args){
-        for (int index= 0; index <= 50; index++){
-            // if(index == 10){
-            //     continue;
-            // }
-            // print even number 
-            if(index%2 == 0){
-                System.out.println(index);
+        
+        for (int index = 0; index <= 50; index++){
+            if(index == 10){
+                break;
             }
-            
+            System.out.println(index);
+            // print even number 
+            // if(index%2 == 0){
+            //     System.out.println(index);
+                
+        
+            }
+        int i = 0;
+        while(i <= 50){
+            if (i == 10){
+                continue;
+            }
+            System.out.println(i);
+            i++;
         }
+        }
+        
     }
-}
+
+
